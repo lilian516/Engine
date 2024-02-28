d884a0932d18f4d89a5375f7dcc27797e1d811e6
EnzoNumber7 <ephilippe@gaming.tech>
2024-02-28 12:00:33 +0100

    Collision

--- Engine/Collider.cpp
+++ Engine/Collider.cpp
@@ -10,4 +10,43 @@ Collider::~Collider() {
 
 void Collider::setCollider() {
 	setComponent(1);
+	m_cCollision = NoCollide;
+}
+
+void Collider::update() {
+	switch (m_cCollision)
+	{
+	case FirstCollide:
+		enterCollision();
+		m_cCollision = Collide;
+		break;
+	case Collide:
+		stayCollision();
+		break;
+	case LastCollide:
+		exitCollision();
+		m_cCollision = LastCollide;
+		break;
+	case NoCollide:
+		noCollision();
+		break;
+	default:
+		break;
+	}
+}
+
+void Collider::enterCollision() {
+
+}
+
+void Collider::stayCollision() {
+
+}
+
+void Collider::exitCollision() {
+
+}
+
+void Collider::noCollision() {
+
 }
--- Engine/Collider.h
+++ Engine/Collider.h
@@ -1,13 +1,25 @@
 #pragma once
 #include "Component.h"
+#include <vector>
+
 class Collider :public Component
 {
+private:
+	enum CollisionState { FirstCollide, Collide, LastCollide, NoCollide };
+	vector<Entity*> m_vColliderEntity;
+	Entity* m_eCollidingEntity;
 public:
 	Collider();
 	~Collider();
 	void setCollider();
-private:
+	void update()override;
 
+	void enterCollision();
+	void stayCollision();
+	void exitCollision();
+	void noCollision();
+
+	CollisionState m_cCollision;
 protected:
 
 };
