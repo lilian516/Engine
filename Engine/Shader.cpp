e592852449ae96f752e6ac3c5ef824fce865be0e
Xevione <bturcat@gaming.tech>
2024-03-14 20:13:53 +0100

    remove com shader

--- Engine/Shader.cpp
+++ Engine/Shader.cpp
@@ -117,8 +117,6 @@ void Shader::initializeShader() {
 }
 vector<CD3DX12_STATIC_SAMPLER_DESC> Shader::GetStaticSamplers()
 {
-	// Applications usually only need a handful of samplers.  So just define them all up front
-	// and keep them available as part of the root signature.  
 
 	CD3DX12_STATIC_SAMPLER_DESC pointWrap(
 		0, // shaderRegister
@@ -127,48 +125,9 @@ vector<CD3DX12_STATIC_SAMPLER_DESC> Shader::GetStaticSamplers()
 		D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
 		D3D12_TEXTURE_ADDRESS_MODE_WRAP); // addressW
 
-	//CD3DX12_STATIC_SAMPLER_DESC pointClamp(
-	//	1, // shaderRegister
-	//	D3D12_FILTER_MIN_MAG_MIP_POINT, // filter
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP); // addressW
-
-	//CD3DX12_STATIC_SAMPLER_DESC linearWrap(
-	//	2, // shaderRegister
-	//	D3D12_FILTER_MIN_MAG_MIP_LINEAR, // filter
-	//	D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
-	//	D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
-	//	D3D12_TEXTURE_ADDRESS_MODE_WRAP); // addressW
-
-	//CD3DX12_STATIC_SAMPLER_DESC linearClamp(
-	//	3, // shaderRegister
-	//	D3D12_FILTER_MIN_MAG_MIP_LINEAR, // filter
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP); // addressW
-
-	//CD3DX12_STATIC_SAMPLER_DESC anisotropicWrap(
-	//	4, // shaderRegister
-	//	D3D12_FILTER_ANISOTROPIC, // filter
-	//	D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
-	//	D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
-	//	D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressW
-	//	0.0f,                             // mipLODBias
-	//	8);                               // maxAnisotropy
-
-	//CD3DX12_STATIC_SAMPLER_DESC anisotropicClamp(
-	//	5, // shaderRegister
-	//	D3D12_FILTER_ANISOTROPIC, // filter
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
-	//	D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressW
-	//	0.0f,                              // mipLODBias
-	//	8);                                // maxAnisotropy
+	
 	return {
-		pointWrap/*, pointClamp,
-		linearWrap, linearClamp,
-		anisotropicWrap, anisotropicClamp */
+		pointWrap
 	};
 }
 
