c74ada386185c28b4e42bf7cd0da20cc9b9410b8
Xevione <bturcat@gaming.tech>
2024-03-15 00:51:11 +0100

    fix collider particles

--- Game/ColliderGame.cpp
+++ Game/ColliderGame.cpp
@@ -6,9 +6,5 @@ ColliderGame::ColliderGame() {
 }
 
 void ColliderGame::enterCollision() {
-	Entity* particle = App::Get()->m_oManager.createEntity();
-	ParticleSystem* system = particle->addComponent<ParticleSystem>();
-	system->setParticleSystem(particle, 1000, 0.1, m_oEntity->getTransform().m_vPosition, App::Get()->m_oManager.m_vShader[0], App::Get()->m_oManager.m_vMesh[0], App::Get()->m_oManager.m_vTexture[0]);
-
 	App::Get()->m_oManager.deleteEntity(m_oEntity);
 }
